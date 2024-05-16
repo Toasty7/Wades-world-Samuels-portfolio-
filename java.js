ae4bfabded1767db3074f304b461635cf0895627
Toasty7 <143848640+Toasty7@users.noreply.github.com>
2024-05-16 15:49:28 -0700

    tgowrnmhgr

--- java.js
+++ java.js
@@ -24,5 +24,46 @@ function showSlide(n) {
     slides[slideIndex].style.display = "block";
 }
 
-console.log
+
+// java scroll 
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("e")
+
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+    let elements = document.querySelectorAll('.slide')
+    // console.log(elements)
+    let x = 0;
+    let istyping = false;
+
+    function scrollfunc() {
+        const height = (window.innerHeight / 5 * 3.5);
+        console.log(height)
+
+        elements.forEach((slide) => {
+            const elementstop = slide.getBoundingClientRect().top;
+            console.log(elementstop)
+            if (elementstop < height) {
+                slide.classList.add('show');
+                slide.classList.remove('slide');
+
+            } else {
+                slide.classList.remove('show');
+
+                slide.classList.add('slide');
+
+            }
+        });
+
+    }
+    window.addEventListener('scroll', scrollfunc)
+    window.addEventListener('touchmove', scrollfunc)
+
+
+
+
+})
 
